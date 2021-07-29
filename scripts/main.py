71a06cc6e4173d7fa561897e2a72ae2f98e10516
LongaBonga <ivan.dobryaev@gmail.com>
2021-07-30 01:32:33 +0300

    delete scheduler form main

--- scripts/main.py
+++ scripts/main.py
@@ -37,7 +37,6 @@ def main():
     parser.add_argument("--model_path", type=str, default='.')
     parser.add_argument("--scheduler_coef", type=float, default=0.97)
 
-    scheduler
 
 
     args = parser.parse_args()
